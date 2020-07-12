1d6277699c97112791e482de9647624cecd1e1cd
fukuoka <fukuoka@eidos.ic.i.u-tokyo.ac.jp>
2020-07-13 05:19:32 +0900

    Add test

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 assert() {
-    ans=$1
-    in=$2
+    in=$1
+    ans=$2
     cargo run $2 > out/asm.S
 
     riscv64-unknown-elf-gcc out/asm.S -o out/run
@@ -20,6 +20,9 @@ assert() {
 
 cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 mkdir -p out
+
 assert 0 0
 assert 10 10
+assert 1+3 4
+assert 1+3+8 12
 
